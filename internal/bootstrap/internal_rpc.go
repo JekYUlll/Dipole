ca44f4af7bf383cd1748a222c07f32af20c25587
JekYUlll <jekyulll@gmail.com>
2026-08-30 01:34:20 +0800

    refactor(core): own agent rpc policy

--- internal/bootstrap/internal_rpc.go
+++ internal/bootstrap/internal_rpc.go
@@ -10,13 +10,11 @@ import (
 	"github.com/JekYUlll/Dipole/internal/application"
 	"github.com/JekYUlll/Dipole/internal/config"
 	platformrpc "github.com/JekYUlll/Dipole/internal/platform/rpc"
+	corepolicy "github.com/JekYUlll/Dipole/internal/services/core/rpcpolicy"
 	agentgrpc "github.com/JekYUlll/Dipole/internal/transport/grpc/agent"
 	grpcauth "github.com/JekYUlll/Dipole/internal/transport/grpc/auth"
 	coregrpc "github.com/JekYUlll/Dipole/internal/transport/grpc/core"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/status"
 )
 
 const (
@@ -148,48 +146,7 @@ func newCoreRPCServer(cfg config.InternalRPC, capability application.CoreCapabil
 		if agentAdapter != nil {
 			agentv1.RegisterAgentCapabilityServiceServer(server, agentAdapter)
 		}
-	}, restrictCoreServiceMethods)
-}
-
-func restrictCoreServiceMethods(ctx context.Context, request any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-	caller, _ := grpcauth.CallerService(ctx)
-	if caller == agentServiceName &&
-		info.FullMethod != agentv1.AgentCapabilityService_AdmitRun_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_CompleteRun_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_FinishRun_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_RequestApproval_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ResolveApproval_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ListConversations_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ReadConversation_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_AuthorizeTaskControl_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ResolveMcpContext_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_BeginMcpToolInvocation_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ResolveMcpToolCommand_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ClaimMcpToolRound_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_FinishMcpToolRound_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_FinishMcpToolInvocation_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_FinishMcpToolInvocationFromRound_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ProjectTaskWorkflowState_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ListTaskWorkflowProjectionSnapshots_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_CreateArtifact_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_MatchEventSubscriptions_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ListContextMemories_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_PublishMcpReadinessEvidence_FullMethodName &&
-		info.FullMethod != agentv1.AgentCapabilityService_ResolveFreshMcpReadinessEvidence_FullMethodName &&
-		info.FullMethod != healthv1.Health_Check_FullMethodName {
-		return nil, status.Error(codes.PermissionDenied, "Agent service is not allowed to call this Core capability")
-	}
-	if caller == searchServiceName &&
-		info.FullMethod != corev1.CoreCapabilityService_ListSearchConversationKeys_FullMethodName &&
-		info.FullMethod != healthv1.Health_Check_FullMethodName {
-		return nil, status.Error(codes.PermissionDenied, "Search service is not allowed to call this Core capability")
-	}
-	if caller == syncServiceName &&
-		info.FullMethod != corev1.CoreCapabilityService_GetGroupMember_FullMethodName &&
-		info.FullMethod != healthv1.Health_Check_FullMethodName {
-		return nil, status.Error(codes.PermissionDenied, "Sync service is not allowed to call this Core capability")
-	}
-	return handler(ctx, request)
+	}, corepolicy.RestrictAgentServiceMethods)
 }
 
 func newInternalRPCServer(cfg config.InternalRPC, address string, allowedCallers []string, register func(*grpc.Server), additionalInterceptors ...grpc.UnaryServerInterceptor) (*InternalRPCServer, error) {
